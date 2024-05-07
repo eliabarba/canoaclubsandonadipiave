0e3eac6a78bb84b463ce204ab260eadc05af2120
Elia Barbaric <e.barbaric@donboscosandona.it>
2024-05-07 12:50:35 +0200

    primo commit

---
+++ components/Present.js
@@ -0,0 +1,14 @@
+'use client';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+export default function Present() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+        
+    </Box>
+  );
+}
